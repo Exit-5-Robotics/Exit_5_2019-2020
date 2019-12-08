2d5c409603f22ae6e18cdb0b4f70d8d9397befed
Rachelle Hu <rachelle.hu123@gmail.com>
2019-12-08 11:46:38 -0500

    Drifting Position Tracking #4

--- Main-code/position-tracking.cpp
+++ Main-code/position-tracking.cpp
@@ -15,9 +15,9 @@ int positionTracker(double zWithoutPi ) {
   Controller1.Screen.setCursor(0, 0);
   Controller1.Screen.print("xDeg: %.0f  yDeg: %.0f", x, y);
   Controller1.Screen.newLine();
-  Controller1.Screen.print("angle: %.3f", z/pi);
+  Controller1.Screen.print("angle: %.3f", z/pi); // tends to drift
   Controller1.Screen.newLine();
-  Controller1.Screen.print("%f", LeftDrive.efficiency());
+  Controller1.Screen.print("%f", LeftDrive.efficiency()); // will try doing smth with efficiency, power, current, velocity for motor
   }
   return 0;
 }
