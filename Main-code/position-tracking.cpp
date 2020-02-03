1a53666d76d02328b36f1458398350179a4a7d9e
Rachelle Hu <rachelle.hu123@gmail.com>
2020-02-02 19:46:18 -0500

    Update position-tracking.cpp

--- Main-code/position-tracking.cpp
+++ Main-code/position-tracking.cpp
@@ -4,53 +4,34 @@
 
 
 double pi = acos(-1);
-int positionTracker( double zWithoutPi ) {
+
+void positionTracker( void ) {
   Controller1.Screen.clearScreen();
-  double x, y, z, ch_z;
-  double cumulative_z = 0;
-  double cur_z = 0;
+  double x, y, z;
   std::cout << std::string(50, '\n');
   int l, r, distanceMoved;
   while (true) {
     l = LeftDrive.position(deg);
     r = RightDrive.position(deg);
     distanceMoved = (l + r)/2;
-    z = zWithoutPi*pi + fmod((((double)l - (double)r) / 2), 1080)*pi / 540; // avg of l and r signed for position, mod 1080 for full rotation, converted to rad
+    z = Inertial.heading();
     x = distanceMoved*cos(z);
     y = distanceMoved*sin(z);
     Controller1.Screen.setCursor(0, 0);
-    Controller1.Screen.print("x: %.0f  y: %.0f ", x, y);
+    Controller1.Screen.print("x: %.3f  y: %.3f ", x, y);
     Controller1.Screen.newLine();
-    Controller1.Screen.print("ang: %.3f", z/pi);
+    Controller1.Screen.print("ang: %.3f", z);
     Controller1.Screen.newLine();
-    Controller1.Screen.print("other ang:?? %.3f", cumulative_z/pi);
-    double eff = (LeftDrive.efficiency() + RightDrive.efficiency())/2;
-    double cur = (LeftDrive.current() + RightDrive.current())/2;
-    double pow = (LeftDrive.power() + RightDrive.power())/2;
-    ch_z = z - cur_z;
-    cur_z = z;
-    Brain.Screen.clearScreen();
-    Brain.Screen.setCursor(8, 8);
-    Brain.Screen.print("%f %f %f", 1.0, 1.0, 1.0); 
-    if (ch_z > 0.05 || ch_z < -0.05) {
-      cumulative_z += ch_z;
-    }
-    std::cout << "e: " << eff << "  c: " << cur << "  p: " << pow << "  ch_z: " << ch_z << "  z: " << z/pi << "  cumulative: " << cumulative_z/pi << std::endl;
+    Controller1.Screen.print("dis: %d", distanceMoved);
   }
-  return x, y, z;
+
 }
+
 
 int goTo( int x_0, int y_0, int z_0, int x, int y, int z, int speed ) {
   Drivetrain.driveFor(fwd, x - x_0, vex::distanceUnits::cm);
-  vex::thread thread( positionTracker, x, y, z );
-  // while (true) 
-  // {
-  //   Brain.Screen.clearScreen();
-  //   Brain.Screen.setCursor(8, 8);
-  //   Brain.Screen.print("%f %f %f", 1.0, 1.0, 1.0); //, x_0, y_0, z_0
-  // }
-  // int distance;
-  // drive(distance, distance, speed, speed);
+  vex::thread thread( &positionTracker );
+  thread.join();
   return 0;
 }
 
